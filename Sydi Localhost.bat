6910a81a2074237437439a9f662b0c59a8d84af8
BrewCityGeek <brewcitygeek@gmail.com>
2017-09-20 10:56:14 -0500

    Update Sydi Localhost.bat
    xml file will be named the computer name instead of localhost.

--- Sydi Localhost.bat
+++ Sydi Localhost.bat
@@ -1,4 +1,4 @@
 @ECHO off
-cscript.exe c:\sydi\sydi-server.vbs -ex -t. -o"c:\sydi\output\localhost.xml"
+cscript.exe c:\sydi\sydi-server.vbs -ex -t. -o"c:\sydi\output\%ComputerName%.xml"
 for /f %%i IN ('dir /b "c:\sydi\output\*.xml"') do call c:\sydi\tools\convert.cmd %%~ni
 for /f %%i IN ('dir /b "c:\sydi\output\*.xml"') do call c:\sydi\tools\transform.cmd %%~ni
